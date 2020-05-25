29432e8caa39e6929a7a4071e9e2f7a0f86ab31b
Rotem Cohen <rotembcohen@gmail.com>
2020-05-25 12:19:38 -0400

    settings for static files

--- purradise/purradise/settings.py
+++ purradise/purradise/settings.py
@@ -82,9 +82,8 @@ DATABASES = {
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -119,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Activate Django-Heroku.
 django_heroku.settings(locals())
