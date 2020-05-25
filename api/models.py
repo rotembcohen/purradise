16239e8715ddf3d67f16d942d4e1c3a659a4aa24
Rotem Cohen <rotembcohen@gmail.com>
2020-05-25 13:43:21 -0400

    add admin config for first model

--- api/models.py
+++ api/models.py
@@ -4,5 +4,6 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
     players_amount = models.PositiveIntegerField(default=2)
+    address = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
