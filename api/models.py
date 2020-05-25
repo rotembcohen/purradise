efa9a19d2513b5cc8062660cdd22670c3d62517c
Rotem Cohen <rotembcohen@gmail.com>
2020-05-25 13:45:14 -0400

    missing default and migrations for address

--- api/models.py
+++ api/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
     players_amount = models.PositiveIntegerField(default=2)
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, default='test')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
