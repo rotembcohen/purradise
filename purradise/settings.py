56a770a45527929a920efc0e0accb0fd0ab89aa0
Rotem Cohen <rotembcohen@gmail.com>
2020-05-25 12:45:06 -0400

    remove secret key

--- purradise/settings.py
+++ purradise/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%a!#o&l#1z97y()x10u+!=#=ct+cdd($$+kz)m!=*bj4fs8wyk'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
